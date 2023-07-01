dc4fd9c11dc47927aaed686b2f8272c5571aabf9
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-07-01 15:02:28 -0500

    Removed whitespace

--- rmv_duplicates.py
+++ rmv_duplicates.py
@@ -5,12 +5,9 @@ Solution for LeetCode problem 83. Remove Duplicates from Sorted List
 
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-
         if head is None:
             return head
-
         temp = head
-
         while temp is not None and temp.next is not None:
             if temp.val == temp.next.val:
                 temp.next = temp.next.next
