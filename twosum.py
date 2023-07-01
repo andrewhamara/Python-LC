cc3db82dd48a1ebdb2129a85de58c22a2d33f109
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-07-01 15:15:47 -0500

    Added documentation and solution

---
+++ twosum.py
@@ -0,0 +1,13 @@
+Author: Andrew Hamara
+
+Solution to LeetCode problem 1. Two Sum
+
+class solution:
+    def twosum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i in range(len(nums)):
+            val = target - nums[i]
+            if val in map:
+                return [map[val], i]
+            else:
+                map[nums[i]] = i
