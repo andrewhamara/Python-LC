e92d46f57fd5240642facc24d5df8f82227b73ee
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-07-06 21:41:38 -0500

    Optimized and changed some file names

--- deleteNodeBST.py
+++ deleteNodeBST.py
@@ -2,4 +2,13 @@ class Solution:
     def deleteNode(self, root:Optional[TreeNode], key:int) -> Optional[Treenode]:
         if not root:
             return None
+        if root.val == key:
+            if not root.left:
+                return right
+            elif not root.right:
+                return left
+            else:
+                temp = right
+                while temp.left:
+                    temp = temp.left
 
--- singleNumber.py
+++ singleNumber.py
@@ -2,9 +2,9 @@
 
 # Solution to leetcode problem 136. Single Number
 
-from collections import Counter
 class Solution:
     def singleNumber(self, nums:List[int]) -> int:
-        for num, count in Counter(nums).items():
-            if count < 2:
-                return num
+        val = 0
+        for x in nums:
+            val = val ^ x
+        return val
