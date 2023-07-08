828ea674e3725450482adbcd5aadf9923f44edaf
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-07-08 13:50:03 -0500

    Cleaned up

--- 206.py
+++ 206.py
@@ -4,13 +4,10 @@
 
 class Solution:
     def reverseList(self, head:Optional[ListNode]) -> Optional[ListNode]:
-        temp = head
-        stack = []
-        while temp != None:
-            stack.append(temp.val)
-            temp = temp.next
-        temp = head
-        while temp != None:
-            temp.val = stack.pop()
-            temp = temp.next
-        return head
+        prev, curr = None, head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
--- 242.py
+++ 242.py
@@ -4,18 +4,18 @@
 
 class Solution:
     def isAnagram(self, s:str, t:str) -> bool:
-        mapOne = {}
-        mapTwo = {}
+        if len(s) != len(t):
+            return False
 
-        for x in s:
-            if x in mapOne:
-                mapOne[x] += 1
-            else:
-                mapOne[x] = 1
+        sMap, tMap = {}, {}
 
-        for x in t:
-            if x in mapTwo:
-                mapTwo[x] += 1
+        for i in range(len(s)):
+            if s[i] in sMap:
+                sMap[s[i]] += 1
             else:
-                mapTwo[x] = 1
-        return mapOne == mapTwo
+                sMap[s[i]] = 1
+            if t[i] in tMap:
+                tMap[t[i]] += 1
+            else:
+                tMap[t[i]] = 1
+        return sMap == tMap
