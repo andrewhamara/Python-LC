9b0f83aeded4c2a7d5c8cb44a7e627d68091e818
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-07-07 19:07:40 -0500

    Changed file name

--- 338.py
+++ 338.py
@@ -4,7 +4,10 @@
 
 class Solution:
     def countBits(self, n:int) -> List[int]:
-        returnList = []
-        for i in range(n+1):
-            returnList.append(str(bin(i)).count('1'))
-        return returnList
+        bitCount = [0] * (n + 1)
+        offset = 1
+        for i in range(1, n + 1):
+            if i == (offset * 2):
+                offset = i
+            bitCount[i] = 1 + bitCount[i - offset]
+        return bitCount
