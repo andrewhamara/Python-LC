6fd6896718ad7f5ec33f7eb6781340ad6e804ef6
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-07-08 20:16:04 -0500

    Added solution

--- 145.py
+++ 145.py
@@ -19,3 +19,11 @@ class Solution:
             if node.right:
                 currentStack.append(node.right)
         return traversal[::-1]
+
+        # This takes advantage of the fact that a preorder traversal returns
+        # nodes in a root-left-right fashion. Using a similar approach to a
+        # preorder solution, but reversing the order of the additions to
+        # the stack, we end up with a  root-right-left order. Since postorder
+        # traversals require a left-right-root traversal, we can simply
+        # reverse the result of the root-right-left list to get the postorder
+        # traversal.
