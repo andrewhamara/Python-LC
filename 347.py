41235495151affd5b8dcb256afc609e047a2108b
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-07-12 23:19:09 -0500

    Added solution

--- 347.py
+++ 347.py
@@ -4,18 +4,20 @@
 
 class Solution:
     def topKFrequent(self, nums:List[int], k:int) -> List[int]:
-        freqMap = {}
-        freqList = [[] for i in range(len(nums) + 1)]
+        if len(nums) == 0:
+            return [nums[0]]
 
-        for x in nums:
-            freqMap[x] = 1 + freqMap.get(x, 0)
+        freqMap = {}
+        freqList = [[] for i in range(0, len(nums) + 1)]
+
+        for n in nums:
+            freqMap[n] = 1 + freqMap.get(n, 0)
         for key, value in freqMap.items():
             freqList[value].append(key)
 
-        res = []
-
-        for i in range(len(freqList) - 1, 0, -1):
+        kFrequent = []
+        for i in range(len(nums), 0, -1):
             for x in freqList[i]:
-                res.append(x)
-                if len(res) == k:
-                    return res
+                kFrequent.append(x)
+                if len(kFrequent) == k:
+                    return kFrequent
