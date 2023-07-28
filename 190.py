8117648b5ec37787d2fcadd0ba9fc621e3696fe3
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-07-28 07:47:40 -0500

    Added solution

--- 190.py
+++ 190.py
@@ -6,8 +6,8 @@ class Solution:
     def reverseBits(self, n:int) -> int:
         rev = 0
 
-        for _ in range(32):
-            rev = rev << 1   # Shift left
+        for _ in range (32):
+            rev = rev << 1 # Make space for new bit to be added
             rev += n & 1
-            n = n >> 1       # Shift right
+            n = n >> 1
         return rev
