741b967b4b6a622223c6a338f822f8e380c690be
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2023-12-04 18:32:24 -0600

    Updated daily 12/4

--- 2264.py
+++ 2264.py
@@ -1,13 +1,10 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
         best = ''
-
         length = len(num)
-        i = 0
-
         for i in range(length - 2):
             if num[i] == num[i+1] == num[i+2]:
                 cur = num[i]
-                if (best and best[0] < cur) or not best:
+                if not best or best[0] < cur:
                     best = cur + cur + cur
         return best
